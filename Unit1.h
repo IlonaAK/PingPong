02bbb8308411aafb9116788dc274016fc0fa4fc0
Ilona Lelek <lelek.ilona.programista@gmail.com>
2021-11-15 13:23:25 +0100

    Added all files

---
+++ Unit1.h
@@ -0,0 +1,49 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <Graphics.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+        TShape *background;
+        TImage *ball;
+        TImage *paddleLeft;
+        TImage *paddleRight;
+        TTimer *L_up;
+        TTimer *L_down;
+        TTimer *R_up;
+        TTimer *R_down;
+        TTimer *Timer_ball;
+        TButton *Button1;
+        TButton *Button2;
+        TLabel *Label1;
+        TLabel *Label2;
+        TLabel *Label3;
+        void __fastcall L_upTimer(TObject *Sender);
+        void __fastcall L_downTimer(TObject *Sender);
+        void __fastcall FormKeyDown(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall FormKeyUp(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall R_upTimer(TObject *Sender);
+        void __fastcall R_downTimer(TObject *Sender);
+        void __fastcall Timer_ballTimer(TObject *Sender);
+        void __fastcall Button2Click(TObject *Sender);
+        void __fastcall Button1Click(TObject *Sender);
+        void __fastcall FormCreate(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+        __fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
